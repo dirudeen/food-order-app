98dfb584b864cbe6c09d761181f08f9ad67bcdb4
Hadiru Colley <dirudeen22@gmail.com>
2023-08-11 19:09:19 +0300

    Added jsx code in mealSummary.js

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MealsSummary = () => {
+    return (
+        <section>
+            <h2>Delicious Food, Delievered To You</h2>
+            <p>
+                choose your favorite meal from out broad selection of available meals and enjoy a delcious luch or dinner at home
+            </p>
+            <p>
+                All our meals are cooked with high-quality ingredients, just-in-time and of course by exprienced chefs!
+            </p>
+        </section>
+    )
+}
+
+export default MealsSummary;
