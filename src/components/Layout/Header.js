fde52804e47319bd93d0eee6099f1ba9412d4a80
Hadiru Colley <dirudeen22@gmail.com>
2023-08-11 18:49:59 +0300

    created HeadCartBtn.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import classes from '../Styles/Header.module.css'
 import mealsImg from "../../assets/meals.jpg"
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return (
         <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <button>Card</button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImg} alt='bowls of meal'/>
