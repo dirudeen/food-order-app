8e89c26814f65ee455d66a292739c81a6b4d0df8
Hadiru Colley <dirudeen22@gmail.com>
2023-08-11 19:46:33 +0300

    fixed ids in DUMMY_MEALS

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,25 +3,25 @@ import classes from '../Styles/AvailableMeals.module.css'
 
 const DUMMY_MEALS = [
     {
-        id: m1,
+        id: 'm1',
         name: 'Benachin',
         description: 'Finest fish, rice and veggies',
         price: 22.99 
     },
     {
-        id: m2,
+        id: 'm2',
         name: 'Domoda',
         description: 'A mandinka specialty',
         price: 40.00 
     },
     {
-        id: m3,
+        id: 'm3',
         name: 'Yassa',
         description: 'Tender chicken with great taste',
         price: 20.50 
     },
     {
-        id: m4,
+        id: 'm4',
         name: 'Fofu',
         description: 'Nigeria\'s finest meal',
         price: 22.99 
