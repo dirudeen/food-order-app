e078c9bdd1c62b2c94cb22e3b76cf0f5813694d1
Hadiru Colley <dirudeen22@gmail.com>
2023-08-11 19:38:00 +0300

    Same as before

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,39 @@
 import React from 'react';
+import classes from '../Styles/AvailableMeals.module.css'
 
+const DUMMY_MEALS = [
+    {
+        id: m1,
+        name: 'Benachin',
+        description: 'Finest fish, rice and veggies',
+        price: 22.99 
+    },
+    {
+        id: m2,
+        name: 'Domoda',
+        description: 'A mandinka specialty',
+        price: 40.00 
+    },
+    {
+        id: m3,
+        name: 'Yassa',
+        description: 'Tender chicken with great taste',
+        price: 20.50 
+    },
+    {
+        id: m4,
+        name: 'Fofu',
+        description: 'Nigeria\'s finest meal',
+        price: 22.99 
+    },
+]
 const AvailableMeals = () => {
-    const DUMMY_MEALS = [
-        {
-            id: m1,
-            name: 'Benachin',
-            description: 'Finest fish, rice and veggies',
-            price: 22.99 
-        },
-        {
-            id: m2,
-            name: 'Domoda',
-            description: 'A mandinka specialty',
-            price: 40.00 
-        },
-        {
-            id: m3,
-            name: 'Yassa',
-            description: 'Tender chicken with great taste',
-            price: 20.50 
-        },
-        {
-            id: m4,
-            name: 'Fofu',
-            description: 'Nigeria\'s finest meal',
-            price: 22.99 
-        },
-    ]
 
     const mealList = DUMMY_MEALS.map((meal) => {
         return <li id={meal.id}>{meal.name}</li>
     })
     return (
-        <section>
+        <section className={classes.meals}>
             <ul>
                 {mealList}
             </ul>
