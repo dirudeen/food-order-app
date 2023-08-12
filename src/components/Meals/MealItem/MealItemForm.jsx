c0334ce4ee44e651936a21976d27566eaafcbd19
Hadiru Colley <dirudeen22@gmail.com>
2023-08-12 12:11:46 +0300

    modifed Form to form element

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -3,10 +3,10 @@ import classes from "../../Styles/MealItemForm.module.css";
 
 const MealItemForm = () => {
   return (
-    <Form className={classes.form}>
+    <form className={classes.form}>
       <input type="number" step={1} />
       <button>+ Add</button>
-    </Form>
+    </form>
   );
 };
 
