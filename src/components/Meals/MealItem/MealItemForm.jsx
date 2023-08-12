efa0e0fadba45b352bd905075ea32fd021ad6e68
Hadiru Colley <dirudeen22@gmail.com>
2023-08-12 12:44:33 +0300

    Added Input.jsx to MealItemForm

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import classes from "../../Styles/MealItemForm.module.css";
+import Input from "../../UI/Input";
 
 const MealItemForm = () => {
   return (
     <form className={classes.form}>
-      <input type="number" step={1} />
+      <Input
+        label="Amount"
+        input={{ id: "amount", type: "text", min: "1", max: "5", step: "1", defaultValue: '1' }}
+      />
       <button>+ Add</button>
     </form>
   );
