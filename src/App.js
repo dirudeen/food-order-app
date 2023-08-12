0047312e36587d989c5050a459cbdc538c659d1d
Hadiru Colley <dirudeen22@gmail.com>
2023-08-12 17:43:51 +0300

    Added useState for toggle the cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler() {
+    return setCartIsShown(true);
+  }
+
+  function HideShowCartHandler() {
+    return setCartIsShown(true)
+  }
+
   return (
     <>
-    <Cart></Cart>
-    <Header></Header>
+    {cartIsShown && <Cart></Cart>}
+    <Header onShowCart={showCartHandler}></Header>
     <Meals></Meals>
     </>
   );
