a72b4c6698591d3151fba1556616875ca7976a85
Hadiru Colley <dirudeen22@gmail.com>
2023-08-12 12:18:32 +0300

    created input.jsx

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const Input = () => {
+  return (
+    <Fragment>
+        <label htmlFor=""></label>
+      <input type="number" name="" id="" />
+    </Fragment>
+  );
+};
+
+export default Input;
