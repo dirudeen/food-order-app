d33c90e4ac8a03a066c32836e32d6b73d444b606
Hadiru Colley <dirudeen22@gmail.com>
2023-08-12 11:41:20 +0300

    modified MealItem.jsx

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import classes from "../../Styles/MealItem.module.css";
 
-const MealItem = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
 
 export default MealItem;
