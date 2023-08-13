4e2762f9cde5441f63fbc2b2257e289d125a022a
Hadiru Colley <dirudeen22@gmail.com>
2023-08-13 10:58:24 +0300

    modified badge number

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,21 @@
-import React , { useContext } from "react";
+import React, { useContext } from "react";
 import classes from "../Styles/HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext);
+  const numberOfItemsInCart = ctx.items.reduce(
+    (acc, item) => acc + item.amount,
+    0
+  );
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{ctx.items.length}</span>
+      <span className={classes.badge}>{numberOfItemsInCart}</span>
     </button>
   );
 };
