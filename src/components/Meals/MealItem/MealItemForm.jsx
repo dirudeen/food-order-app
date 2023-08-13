2ff2fb798e954ec2bdad999179e1f06b30b5daa7
Hadiru Colley <dirudeen22@gmail.com>
2023-08-13 12:16:26 +0300

    added onSubmit event

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useRef} from "react";
 import classes from "../../Styles/MealItemForm.module.css";
 import Input from "../../UI/Input";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
+  const CartInputRef = useRef()
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredAmount = CartInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount
+
+    if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+      return;
+    }
+
+    props.onAddAmount = enteredAmountNumber
+  }
+  
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={CartInputRef}
         label="Amount"
-        input={{ id: "amount", type: "text", min: "1", max: "5", step: "1", defaultValue: '1' }}
+        input={{
+          id: "amount",
+          type: "text",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
       />
       <button>+ Add</button>
     </form>
