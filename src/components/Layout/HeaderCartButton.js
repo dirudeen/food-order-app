9dfb36270f8cac349aa65a002ad9329ba37a30a4
Hadiru Colley <dirudeen22@gmail.com>
2023-08-13 10:35:43 +0300

    Added badge number dynamically

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "../Styles/HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{ctx.items.length}</span>
     </button>
   );
 };
