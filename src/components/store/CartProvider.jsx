a9eec5b4f73ee9157994e128ca482114c7c3b2a9
Hadiru Colley <dirudeen22@gmail.com>
2023-08-13 10:23:27 +0300

    created  CartProvider.jsx

---
+++ src/components/store/CartProvider.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import CartContext from './cart-context';
+
+const CartProvider = () => {
+    const values = {
+        item: [],
+        totalAmount: 0,
+        
+    }
+    return (
+        <CartContext>
+        
+        </CartContext>
+    );
+}
+
+export default CartProvider;
