27f032633ffb4b16ebc5672a1f30677798156465
Hadiru Colley <dirudeen22@gmail.com>
2023-08-22 12:45:49 +0300

    Added error handling mechanism for fetching data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,19 +2,21 @@ import React, { useEffect, useState } from "react";
 import classes from "../Styles/AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import { SpinnerDiamond } from "spinners-react";
 
 const AvailableMeals = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [meals, setMeals] = useState([])
+  const [meals, setMeals] = useState([]);
 
   const fetchMealsData = async () => {
-    setIsLoading(true)
-    setError(null)
+    setIsLoading(true);
+    setError(null);
     try {
       const response = await fetch(
         "https://tasks-data-17dc7-default-rtdb.firebaseio.com/meals.json"
       );
+      
       if (!response.ok) {
         throw new Error("opps! something went wrong");
       }
@@ -29,35 +31,46 @@ const AvailableMeals = () => {
           price: mealsData[key].price,
         });
       }
-     setMeals(loadedData)
-    
+      setMeals(loadedData);
     } catch (error) {
       setError(error.message);
     }
-    setIsLoading(false)
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchMealsData();
   }, []);
 
-       const mealList = meals.map((meal) => {
-         return (
-           <MealItem
-             id={meal.id}
-             key={meal.id}
-             name={meal.name}
-             description={meal.description}
-             price={meal.price}
-           />
-         );
-       });
+  const mealList = meals.map((meal) => {
+    return (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
+  });
 
+  let content = <p>No meals available</p>;
 
+  if (meals.length > 0) {
+    content = <ul>{mealList}</ul>;
+  }
+
+  if (isLoading) {
+    content = <SpinnerDiamond />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
 
   return (
     <section className={classes.meals}>
-      <Card><ul>{mealList}</ul></Card>
+      <Card>{content}</Card>
     </section>
   );
 };
